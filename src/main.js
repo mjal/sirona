645a38a23dfbb41288df9cab717566065152b5f9
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-05-09 15:35:09 +0200

    Log crash of the verifier to the critcal section

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 // TODO: Use `check` instead of assert and logSuccess
-import { readFile } from "./utils.js";
+import { readFile, logError } from "./utils.js";
 import load from "./load.js";
 import checkFiles from "./checkFiles.js";
 import checkSetup from "./checkSetup.js";
 import checkBallot from "./checkBallot.js";
 
 export default function(fileEntries) {
-  let state = load(fileEntries.map(readFile));
-
-  checkFiles(state);
-  checkSetup(state);
-  for (let i = 0; i < state.ballots.length; i++) {
-    checkBallot(state, state.ballots[i]);
+  try {
+    let state = load(fileEntries.map(readFile));
+    checkFiles(state);
+    checkSetup(state);
+    for (let i = 0; i < state.ballots.length; i++) {
+      checkBallot(state, state.ballots[i]);
+    }
+  } catch (e) {
+    logError("critical", "Something wrong happened.");
   }
 }
