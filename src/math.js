ac3ed2ad75130ffd925d6077dda835e797ba159e
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-05-16 10:51:34 +0200

    Reorder math constants

--- src/math.js
+++ src/math.js
@@ -1,6 +1,10 @@
 import { ed25519 } from "@noble/curves/ed25519";
 
 export const g = ed25519.ExtendedPoint.BASE;
+export const one = ed25519.ExtendedPoint.fromHex(
+  "0100000000000000000000000000000000000000000000000000000000000000",
+);
+
 export const q = 2n ** 255n - 19n;
 export const l = BigInt(
   "0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed",
@@ -12,10 +16,6 @@ export const rev = (hexStr) => {
     .reverse()
     .join("");
 };
-
-export const one = ed25519.ExtendedPoint.fromHex(
-  rev("0000000000000000000000000000000000000000000000000000000000000001"),
-);
 
 export const erem = (a, b) => {
   let remainder = a % b;
