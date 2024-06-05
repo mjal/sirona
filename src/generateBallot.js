de63f07a1494d6dccfa4ca7cb97278df534e0530
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-05 21:37:36 +0200

    WIP: Encrypt Ballot

--- src/generateBallot.js
+++ src/generateBallot.js
@@ -13,6 +13,59 @@ export default function (state, credential, choices) {
   console.log(signature(nPrivateCredential, H));
 }
 
+export function encrypt(state, plaintext) {
+  console.assert(plaintext === 0 || plaintext === 1, "Invalid plaintext");
+
+  const r = rand();
+  const pY = parsePoint(state.setup.payload.election.public_key);
+  const gPowerM = plaintext === 0 ? zero : g;
+  const pAlpha = g.multiply(r);
+  const pBeta = pY.multiply(r).add(gPowerM);
+
+  let iproof = [];
+  let commitments = []; // As, Bs
+
+  for (let i = 0; i < 2; i++) {
+    iproof.push({
+      challenge: rand().toString(),
+      response: rand().toString(),
+    });
+  }
+
+  for (let i = 0; i < 2; i++) {
+    const [pA, pB] = formula1(pY, pAlpha, pBeta,
+      BigInt(iproof[i].challenge),
+      BigInt(iproof[i].response), i);
+    commitments.push([pA, pB]);
+  }
+
+  const w = rand();
+  for (let i = 0; i < 2; i++) {
+    if (i === ciphertext) {
+      const pA = g.multiply(w);
+      const pB = pY.multiply(w);
+      commitments[i] = [pA, pB];
+    }
+  }
+  commitments = commitments.flat();
+
+  let nSumChallenge = BigInt(0);
+  for (let i = 0; i < 2; i++) {
+    if (plaintext !== i) {
+      nSumChallenge = mod(nSumChallenge + BigInt(iproof[i].challenge), L);
+    }
+  }
+
+  return {
+    r,
+    ciphertext: {
+      alpha: pAlpha.toHex(),
+      beta: pBeta.toHex(),
+    },
+    iproof,
+  };
+}
+
 export function signature(nPrivateCredential, hash) {
   const w = rand();
   const pA = g.multiply(w);
