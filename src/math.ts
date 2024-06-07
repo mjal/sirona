58747ce787494f34c9a0ec3027a454dccc2950b1
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-07 13:39:30 +0200

    Remove rand() mock

--- src/math.ts
+++ src/math.ts
@@ -57,10 +57,9 @@ export function modInverse(a: bigint, m: bigint): bigint {
 }
 
 export function rand() : bigint {
-  return mod(BigInt("0x133"), L);
-  //const bitArray = sjcl.random.randomWords(8);
-  //const hNumber = sjcl.codec.hex.fromBits(bitArray);
-  //return mod(BigInt("0x" + hNumber), L);
+  const bitArray = sjcl.random.randomWords(8);
+  const hNumber = sjcl.codec.hex.fromBits(bitArray);
+  return mod(BigInt("0x" + hNumber), L);
 }
 
 export function formula(p1: point, e1: bigint, p2: point, e2: bigint) {
