5ae64fc652e7df6c2033747cb7a20b5035d5057c
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-11 18:27:21 +0200

    Show election questions

--- src/components/ElectionInfo.vue
+++ src/components/ElectionInfo.vue
@@ -5,6 +5,8 @@ const props = defineProps(["state"]);
 const election = props.state.setup?.payload.election;
 const countBallots = props.state.ballots?.length;
 const fingerprint = props.state.setup?.fingerprint;
+
+console.log(election);
 </script>
 
 <template>
@@ -15,4 +17,19 @@ const fingerprint = props.state.setup?.fingerprint;
     <p>Election fingerprint: {{ fingerprint }}</p>
     <p>Number of ballots: {{ countBallots }}</p>
   </div>
+
+  <template v-if="election">
+    <template v-for="(question, index) in election.questions" v-bind:key="index">
+      <div class="uk-card uk-card-default uk-card-body uk-margin">
+        <h3 class="uk-card-title uk-margin">{{ question.question }}</h3>
+        <h4 v-for="(answer, index) in question.answers" v-bind:key="index">
+          Answer {{ index }} : {{ answer }}
+        </h4>
+        <div class="uk-margin">
+          <p>Selection between {{ question.min }} and {{ question.max }} choices</p>
+          <p>{{ question.blank ? "Blank vote allowed" : "Blank vote not allowed" }}</p>
+        </div>
+      </div>
+    </template>
+  </template>
 </template>
