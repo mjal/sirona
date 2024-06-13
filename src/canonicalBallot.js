3ff8fa60d178983ac583f9ee3ea8691d5a2af3b2
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-13 22:30:05 +0200

    Refactor ballot serialization

--- src/canonicalBallot.js
+++ src/canonicalBallot.js
@@ -1,8 +1,21 @@
+function copyCiphertext(ciphertext) {
+  return {
+    alpha: ciphertext.alpha,
+    beta: ciphertext.beta,
+  };
+}
+
+function copyProof(proof) {
+  return {
+    challenge: proof.challenge,
+    response: proof.response,
+  };
+}
+
 export default function (ballot) {
-  // On most implementations, the order of the fields in the
-  // serialization correspond to the order of insertion. This
-  // is not guaranteed by the JSON standard, but it is guaranteed
-  // by JSON.stringify in most implementations.
+  // The order of the fields in the JSON.stringify serialization
+  // correspond to the order of insertion.
+  // This is not guaranteed by but is the case in every tested js engines.
   const obj = {
     election_uuid: ballot.election_uuid,
     election_hash: ballot.election_hash,
@@ -10,60 +23,34 @@ export default function (ballot) {
     answers: ballot.answers.map((answer) => {
       let obj = {};
       if (Array.isArray(answer.choices)) {
-        obj.choices = answer.choices.map((choice) => {
-          return {
-            alpha: choice.alpha,
-            beta: choice.beta,
-          };
-        });
+        obj.choices = answer.choices.map(copyCiphertext);
       } else {
-        obj.choices = {
-          alpha: answer.choices.alpha,
-          beta: answer.choices.beta,
-        };
+        obj.choices = copyCiphertext(answer.choices);
       }
       if (answer.proof) {
-        obj.proof = {
-          challenge: answer.proof.challenge,
-          response: answer.proof.response,
-        };
+        obj.proof = copyProof(answer.proof);
       }
       if (answer.individual_proofs) {
         obj.individual_proofs = answer.individual_proofs.map((iproof) => {
-          return iproof.map((proof) => {
-            return {
-              challenge: proof.challenge,
-              response: proof.response,
-            };
-          });
+          return iproof.map(copyProof);
         });
       }
       if (answer.overall_proof) {
         if (Array.isArray(answer.overall_proof)) {
-          obj.overall_proof = answer.overall_proof.map((proof) => {
-            return {
-              challenge: proof.challenge,
-              response: proof.response,
-            };
-          });
+          obj.overall_proof = answer.overall_proof.map(copyProof);
         } else {
-          obj.overall_proof = {
-            challenge: answer.overall_proof.challenge,
-            response: answer.overall_proof.response,
-          };
+          obj.overall_proof = copyProof(answer.overall_proof);
         }
       }
       if (answer.blank_proof !== undefined) {
-        obj.blank_proof = answer.blank_proof.map((proof) => {
-          return {
-            challenge: proof.challenge,
-            response: proof.response,
-          };
-        });
+        obj.blank_proof = answer.blank_proof.map(copyProof);
       }
       return obj;
     }),
-    signature: ballot.signature,
+    signature: {
+      hash: ballot.signature.hash,
+      proof: copyProof(ballot.signature.proof)
+    }
   };
   return obj;
 }
