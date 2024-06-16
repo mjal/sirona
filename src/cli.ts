0806042e07eea08e6d1a4fba9dd8d9f62a186a43
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-16 17:47:24 +0200

    CLI: Migrate from deno to ts-node

--- src/cli.ts
+++ src/cli.ts
@@ -1,41 +1,46 @@
-import { log } from "./logger";
-import { TarReader } from "./tarReader.js";
-import check from "./check.js";
-import fs from 'node:fs'
+import { TarReader } from "./tarReader";
 import { getLogs, getBallotLogs } from "./logger";
+import check from "./check";
 
-const filePath = Deno.args[0];
+// Import fs
+import fs from "fs"; // Without star
 
-const data = await fs.promises.readFile(filePath);
+const main = async () => {
+  const filePath = process.argv[2];
+  const data = await fs.promises.readFile(filePath);
 
-const tarReader = new TarReader(data);
-const files = tarReader.getFiles();
-const state = await check(files);
-let errors = 0;
+  const tarReader = new TarReader(data);
+  const files = tarReader.getFiles();
+  const state = await check(files);
 
-const sectionLogs = getLogs();
-const sections = Object.keys(sectionLogs);
-for (let i = 0; i < sections.length; i++) {
-  const logs = sectionLogs[sections[i]];
-  console.log("=== " + sections[i] + " ===");
-  for (let j = 0; j < logs.length; j++) {
-    if (!logs[j].pass) { errors++; }
-    const prefix = logs[j].pass ? "✅" : "❌";
-    console.log(prefix + logs[j].message);
+  let errors = 0;
+
+  const sectionLogs = getLogs();
+  const sections = Object.keys(sectionLogs);
+  for (let i = 0; i < sections.length; i++) {
+    const logs = sectionLogs[sections[i]];
+    console.log("=== " + sections[i] + " ===");
+    for (let j = 0; j < logs.length; j++) {
+      if (!logs[j].pass) { errors++; }
+      const prefix = logs[j].pass ? "✅" : "❌";
+      console.log(prefix + logs[j].message);
+    }
   }
+  
+  const ballotLogs = getBallotLogs();
+  const ballotKeys = Object.keys(ballotLogs);
+  console.log("=== BALLOTS ===");
+  for (let i = 0; i < ballotKeys.length; i++) {
+    const logs = ballotLogs[ballotKeys[i]];
+    console.log("=== " + ballotKeys[i] + " ===");
+    for (let j = 0; j < logs.length; j++) {
+      if (!logs[j].pass) { errors++; }
+      const prefix = logs[j].pass ? "✅" : "❌";
+      console.log(prefix + logs[j].message);
+    }
+  }
+
+  process.exit(errors > 0 ? 1 : 0);
 }
 
-const ballotLogs = getBallotLogs();
-const ballotKeys = Object.keys(ballotLogs);
-console.log("=== BALLOTS ===");
-for (let i = 0; i < ballotKeys.length; i++) {
-  const logs = ballotLogs[ballotKeys[i]];
-  console.log("=== " + ballotKeys[i] + " ===");
-  for (let j = 0; j < logs.length; j++) {
-    if (!logs[j].pass) { errors++; }
-    const prefix = logs[j].pass ? "✅" : "❌";
-    console.log(prefix + logs[j].message);
-  }
-}
-
-Deno.exit(errors > 0 ? 1 : 0);
+main();
