f55c451005aae6996734a8d405052f183c8b0291
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-19 04:12:30 +0200

    Parse/Serialize NonZeroProof

--- src/nonZeroProof.ts
+++ src/nonZeroProof.ts
@@ -13,3 +13,19 @@ export namespace Serialized {
     response: [string, string];
   }
 }
+
+export function serialize(proof: t): Serialized.t {
+  return {
+    commitment: Point.serialize(proof.pCommitment),
+    challenge: proof.nChallenge.toString(),
+    response: [proof.nResponse[0].toString(), proof.nResponse[1].toString()],
+  };
+}
+
+export function parse(proof: Serialized.t): t {
+  return {
+    pCommitment: Point.parse(proof.commitment),
+    nChallenge: BigInt(proof.challenge),
+    nResponse: [BigInt(proof.response[0]), BigInt(proof.response[1])],
+  };
+}
