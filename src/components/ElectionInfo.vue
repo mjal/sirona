edb31500b738cb010776791bbdec0b34b9b26882
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-26 16:53:19 +0200

    Question UI

--- src/components/ElectionInfo.vue
+++ src/components/ElectionInfo.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from "vue";
+import Question from "./Question.vue";
 
 const props = defineProps(["state", "logs", "ballotLogs"]);
 const election = props.state.setup?.payload.election;
@@ -21,61 +22,51 @@ const hasError = computed(() => {
 </script>
 
 <template>
-    <table class="uk-table uk-table-striped">
-      <caption>Election infos</caption>
-      <tbody>
-        <tr>
-          <td>Verification Status</td>
-          <td>
-            <span v-if="hasError" class="uk-label uk-label-danger">Error</span>
-            <span v-else class="uk-label uk-label-success">Success</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Election Status</td>
-          <td>
-            <span v-if="hasResult" class="uk-label uk-label-success">Finished</span>
-            <span v-else class="uk-label">In progress</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Name</td>
-          <td>{{ election.name }}</td>
-        </tr>
-        <tr>
-          <td>Description</td>
-          <td>{{ election.description }}</td>
-        </tr>
-        <tr>
-          <td>UUID</td>
-          <td>{{ election.uuid }}</td>
-        </tr>
-        <tr>
-          <td>Fingerprint</td>
-          <td>{{ fingerprint }}</td>
-        </tr>
-        <tr>
-          <td>Number of ballots</td>
-          <td>{{ countBallots }}</td>
-        </tr>
-      </tbody>
-    </table>
-
-  <!--div class="uk-card uk-card-default uk-card-body uk-margin" v-if="election">
-  </div-->
+  <table class="uk-table uk-table-striped">
+    <caption>Election infos</caption>
+    <tbody>
+      <tr>
+        <td>Verification Status</td>
+        <td>
+          <span v-if="hasError" class="uk-label uk-label-danger">Error</span>
+          <span v-else class="uk-label uk-label-success">Success</span>
+        </td>
+      </tr>
+      <tr>
+        <td>Election Status</td>
+        <td>
+          <span v-if="hasResult" class="uk-label uk-label-success">Finished</span>
+          <span v-else class="uk-label">In progress</span>
+        </td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td>{{ election.name }}</td>
+      </tr>
+      <tr>
+        <td>Description</td>
+        <td>{{ election.description }}</td>
+      </tr>
+      <tr>
+        <td>UUID</td>
+        <td>{{ election.uuid }}</td>
+      </tr>
+      <tr>
+        <td>Fingerprint</td>
+        <td>{{ fingerprint }}</td>
+      </tr>
+      <tr>
+        <td>Number of ballots</td>
+        <td>{{ countBallots }}</td>
+      </tr>
+    </tbody>
+  </table>
 
   <template v-if="election">
-    <template v-for="(question, index) in election.questions" v-bind:key="index">
-      <div class="uk-card uk-card-default uk-card-body uk-margin">
-        <h3 class="uk-card-title uk-margin">{{ question.question }}</h3>
-        <h4 v-for="(answer, index) in question.answers" v-bind:key="index">
-          Answer {{ index }} : {{ answer }}
-        </h4>
-        <div class="uk-margin">
-          <p>Selection between {{ question.min }} and {{ question.max }} choices</p>
-          <p>{{ question.blank ? "Blank vote allowed" : "Blank vote not allowed" }}</p>
-        </div>
-      </div>
-    </template>
+    <ul uk-accordion>
+      <template v-for="(question, index) in election.questions" v-bind:key="index">
+        <Question :question="question" />
+      </template>
+    </ul>
   </template>
 </template>
---
+++ src/components/Question.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["question"]);
+
+const type = computed(() => {
+  return props.question.type === undefined ? "Homomorphic" : props.question.type;
+});
+
+const supported = computed(() => {
+  return props.question.type === undefined || props.question.type === "Lists";
+});
+
+
+console.log(props.question);
+</script>
+
+<template>
+  <li>
+    <a class="uk-accordion-title" href>
+      Question: "{{ question.question }}"
+      <span class="uk-label">{{ type }}</span>
+      <span v-if="supported" class="uk-label uk-label-success">Supported</span>
+      <span v-else class="uk-label uk-label-danger">Unsupported</span>
+    </a>
+    
+    <div class="uk-accordion-content">
+      <h4 v-for="(answer, index) in question.answers" v-bind:key="index">
+        Answer {{ index }} : {{ answer }}
+      </h4>
+      <div class="uk-margin">
+        <p>Selection between {{ question.min }} and {{ question.max }} choices</p>
+        <p>{{ question.blank ? "Blank vote allowed" : "Blank vote not allowed" }}</p>
+      </div>
+    </div>
+  </li>
+</template>
