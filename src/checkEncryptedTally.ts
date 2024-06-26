6767af18127fe0ca8eac2a5375136f898c6479bd
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-26 15:55:42 +0200

    Lists questions (3/3): Check EncryptedTally

--- src/checkEncryptedTally.ts
+++ src/checkEncryptedTally.ts
@@ -7,31 +7,63 @@ export default function (state) {
   const questions = state.setup.payload.election.questions;
   const encryptedTally = [];
   for (let i = 0; i < questions.length; i++) {
-    const answers = questions[i].answers || [];
-    const row = answers.map((_answer) => {
-      return { alpha: zero, beta: zero };
-    });
-    if (questions[i].blank) {
-      row.push({ alpha: zero, beta: zero });
+    if (questions[i].type === undefined) { // question_h
+      const row = questions[i].answers.map((_) => {
+        return { alpha: zero, beta: zero };
+      });
+      if (questions[i].blank) {
+        row.push({ alpha: zero, beta: zero });
+      }
+      encryptedTally.push(row);
+    } else if (questions[i].type === "Lists") {
+      const matrix = questions[i].value.answers.map((l) => {
+        return l.map((_) => {
+          return { alpha: zero, beta: zero };
+        });
+      });
+      encryptedTally.push(matrix);
+    } else {
+      log("encryptedTally", false, "Unsupported question type");
+      encryptedTally.push([]);
     }
-    encryptedTally.push(row);
   }
 
   for (let i = 0; i < ballots.length; i++) {
-    for (let j = 0; j < encryptedTally.length; j++) {
-      const answer = ballots[i].payload.answers[j];
-      for (let k = 0; k < encryptedTally[j].length; k++) {
-        const pAlpha = parsePoint(answer.choices[k].alpha);
-        const pBeta = parsePoint(answer.choices[k].beta);
-        const weight = state.credentialsWeights.find(
-          (line) => line.credential === ballots[i].payload.credential,
-        ).weight;
-        encryptedTally[j][k].alpha = encryptedTally[j][k].alpha.add(
-          pAlpha.multiply(BigInt(weight)),
-        );
-        encryptedTally[j][k].beta = encryptedTally[j][k].beta.add(
-          pBeta.multiply(BigInt(weight)),
-        );
+    for (let j = 0; j < questions.length; j++) {
+      if (questions[j].type === undefined) { // question_h
+        const answer = ballots[i].payload.answers[j];
+        for (let k = 0; k < encryptedTally[j].length; k++) {
+          const pAlpha = parsePoint(answer.choices[k].alpha);
+          const pBeta = parsePoint(answer.choices[k].beta);
+          const weight = state.credentialsWeights.find(
+            (line) => line.credential === ballots[i].payload.credential,
+          ).weight;
+          encryptedTally[j][k].alpha = encryptedTally[j][k].alpha.add(
+            pAlpha.multiply(BigInt(weight)),
+          );
+          encryptedTally[j][k].beta = encryptedTally[j][k].beta.add(
+            pBeta.multiply(BigInt(weight)),
+          );
+        }
+      } else if (questions[j].type === "Lists") {
+        const answer = ballots[i].payload.answers[j];
+        for (let k = 0; k < encryptedTally[j].length; k++) {
+          for (let l = 0; l < encryptedTally[j][k].length; l++) {
+            const pAlpha = parsePoint(answer.choices[k][l].alpha);
+            const pBeta = parsePoint(answer.choices[k][l].beta);
+            const weight = state.credentialsWeights.find(
+              (line) => line.credential === ballots[i].payload.credential,
+            ).weight;
+            encryptedTally[j][k][l].alpha = encryptedTally[j][k][l].alpha.add(
+              pAlpha.multiply(BigInt(weight)),
+            );
+            encryptedTally[j][k][l].beta = encryptedTally[j][k][l].beta.add(
+              pBeta.multiply(BigInt(weight)),
+            );
+          }
+        }
+      } else {
+        log("encryptedTally", false, "Unsupported question type");
       }
     }
   }
@@ -45,6 +77,16 @@ export default function (state) {
             et[i][j].beta === rev(encryptedTally[i][j].beta.toHex()),
           "Encrypted tally microballot correspond to the weighted sum of all ballots",
         );
+      }
+    } else if (questions[i].type === "Lists") {
+      for (let j = 0; j < et[i].length; j++) {
+        for (let k = 0; k < et[i][j].length; k++) {
+          log("encryptedTally",
+            et[i][j][k].alpha === rev(encryptedTally[i][j][k].alpha.toHex()) &&
+              et[i][j][k].beta === rev(encryptedTally[i][j][k].beta.toHex()),
+            "Encrypted tally microballot correspond to the weighted sum of all ballots",
+          );
+        }
       }
     } else {
       log("encryptedTally", false, "Unsupported question type");
