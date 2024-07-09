93bc5618e2b4c0039c1e5f8e46bda3d84048e5cd
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-07-09 15:21:44 +0200

    AnswerNH: Check proof

--- src/AnswerNH.ts
+++ src/AnswerNH.ts
@@ -1,9 +1,10 @@
+import * as Point from "./point";
 import * as Proof from "./proof";
 import * as Ciphertext from "./ciphertext";
 import * as Election from "./election";
 import * as Question from "./question";
 import * as Ballot from "./ballot";
-import { isValidPoint } from "./math";
+import { isValidPoint, formula, Hraweg } from "./math";
 
 // -- Types
 
@@ -31,12 +32,29 @@ export function check(
   if (!checkValidPoints(answer)) {
     throw new Error("Invalid curve points");
   }
-  throw new Error("NonHomomorphic questions not fully implemented yet");
-
+  if (!checkProof(election, electionFingerprint, ballot, question, answer)) {
+    throw new Error("Invalid proof");
+  }
   return true;
 }
 
-export function checkValidPoints(answer: Serialized.t) {
+function checkValidPoints(answer: Serialized.t) {
   const ct = Ciphertext.parse(answer.choices);
   return (isValidPoint(ct.pAlpha) && isValidPoint(ct.pBeta));
+}
+
+function checkProof(
+  election: Election.t,
+  electionFingerprint: string,
+  ballot: Ballot.t,
+  _question: Question.QuestionNH.t,
+  answer: Serialized.t,
+) {
+  const y = Point.parse(election.public_key);
+  const ct = Ciphertext.parse(answer.choices);
+  const proof = Proof.parse(answer.proof);
+  const A = formula(Point.g, proof.nResponse, ct.pAlpha, proof.nChallenge);
+  const S = `${electionFingerprint}|${ballot.credential}`;
+
+  return (Hraweg(S, y, ct.pAlpha, ct.pBeta, A) === proof.nChallenge);
 }
--- src/math.ts
+++ src/math.ts
@@ -131,3 +131,17 @@ export function Hlproof(S: string, ...commitments: Array<Point.t>) {
 export function Hdecrypt(S: string, ...commitments: Array<Point.t>) {
   return H(`decrypt|${S}`, ...commitments);
 }
+
+export function Hraweg(
+  S: string,
+  y: Point.t,
+  alpha: Point.t,
+  beta: Point.t,
+  A: Point.t
+) {
+  let prefix = `raweg|${S}|`;
+  prefix += Point.serialize(y) + ",";
+  prefix += Point.serialize(alpha) + ",";
+  prefix += Point.serialize(beta)
+  return H(prefix, A);
+}
