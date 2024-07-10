0143f63595e6118d69799a7314ba74fcb32964fb
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-07-10 17:23:45 +0200

    More utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,17 @@
 export const assert = console.assert;
 
-export const map2 = (a, fn) => a.map((b) => b.map(fn));
-export const map3 = (a, fn) => a.map((b) => map2(b, fn));
+export const map2 = (a: any, fn: any) => a.map((b: any) => b.map(fn));
+export const map3 = (a: any, fn: any) => a.map((b: any) => map2(b, fn));
 
-export async function _async(f, ...args) {
+export async function _async(f: any, ...args: any) {
   return new Promise((resolve, _reject) => {
     setTimeout(() => {
       const res = f(...args);
       resolve(res);
     }, 0);
   });
+}
+
+export function error(str: string) {
+  throw new Error(str);
 }
