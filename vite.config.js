868d098cb6a0091a2c27237f3cd175c70a817782
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-07-11 12:50:08 +0200

    update base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/belenios-js-verifier/",
+  base: "/sirona/",
   plugins: [vue()],
 });
