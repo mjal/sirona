d43a85ccf0434387c8cb154872fcb439696c77db
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-07-28 17:53:18 +0200

    PartialDecryption type

--- src/check.ts
+++ src/check.ts
@@ -4,9 +4,9 @@ import load from "./load";
 import checkFiles from "./checkFiles";
 import checkSetup from "./checkSetup";
 import checkEncryptedTally from "./checkEncryptedTally";
-import checkPartialDecryptions from "./checkPartialDecryptions";
 import * as Ballot from "./ballot";
 import * as Shuffle from "./Shuffle";
+import * as PartialDecryption from "./PartialDecryption";
 import * as Result from "./Result";
 
 export default async function (fileEntries) {
@@ -23,20 +23,20 @@ export default async function (fileEntries) {
     if (!state.encryptedTally) return state;
     await _async(checkEncryptedTally, state);
 
-    // Check shuffles
     let tally = state.encryptedTally.payload.encrypted_tally;
     for (let i = 0; i < state.shuffles.length; i++) {
       await _async(Shuffle.verify, state, state.shuffles[i], tally);
       tally = state.shuffles[i].payload.payload.ciphertexts;
     }
 
-    if (state.partialDecryptions.length > 0) {
-      await _async(checkPartialDecryptions, state);
+    for (let i = 0; i < state.partialDecryptions.length; i++) {
+      await _async(PartialDecryption.verify, state, state.partialDecryptions[i]);
     }
 
     if (state.result) {
       await _async(Result.verify, state);
     }
+
     log("top", true, "Verification done.");
     return state;
   } catch (e) {
