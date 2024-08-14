82fb99693efdf323f0c50fa1273b48cdab8345dc
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-08-14 20:57:35 +0200

    cli: setup generate-credentials

--- src/cli/sirona-setup.ts
+++ src/cli/sirona-setup.ts
@@ -1,15 +1,16 @@
 import fs from "fs";
 import { Command } from "commander";
+import * as Credential from "../Credential";
 
+
+const b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 const program = new Command();
 
 program.command("generate-token")
   .action(() => {
-    const chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
-  
     let uuid = Array.from({ length: 14 }, (_, i) => {
-      const randomIndex = Math.floor(Math.random() * chars.length);
-      return chars[randomIndex];
+      const randomIndex = Math.floor(Math.random() * b58chars.length);
+      return b58chars[randomIndex];
     }).join('');
   
     console.log(uuid);
@@ -17,14 +18,38 @@ program.command("generate-token")
 
 program.command("generate-credentials")
   .option("--file <FILE>", "Read  identities  from  FILE.  One credential will be generated for each line of FILE.")
+  .option("--uuid <UUID>", "UUID of the election.")
   .action(async function (options) {
     const data = await fs.promises.readFile(options.file);
-    const lines = data.toString().split("\n");
-    lines.forEach((line) => {
+    const lines = data.toString().split("\n").filter((line) => line.length > 0);
+
+    const privcreds = Object.fromEntries(lines.map((line) => {
       const [email, id, weight] = line.split(",");
-      console.log(`${id} ${email} ${weight}`);
+
+      let privcred = Array.from({ length: 24 }, (_, i) => {
+        if (i === 5 || i === 12 || i === 18) {
+          return '-';
+        }
+        const randomIndex = Math.floor(Math.random() * b58chars.length);
+        return b58chars[randomIndex];
+      }).join('');
+
+      return [id, privcred];
+    }));
+
+    const pubcreds = lines.map((line, i) => {
+      const [email, id, weight] = line.split(",");
+      const privcred = privcreds[id];
+
+      const { hPublicCredential } = Credential.derive(options.uuid, privcred);
+
+      return `${hPublicCredential},${weight},${id}`
     });
-    console.log(data);
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    await fs.promises.writeFile(`${timestamp}.privcreds`, JSON.stringify(privcreds, null, 0));
+    await fs.promises.writeFile(`${timestamp}.pubcreds`, JSON.stringify(pubcreds, null, 0));
   });
+
 
 program.parseAsync(process.argv);
