6e5d4797f9c1c9848713fbd55f0f223f5190e30e
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-08-15 15:52:17 +0200

    Remove disclaimer about hungarian notation

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,6 @@
  * This software is licensed under the GNU Affero General Public License (AGPL).
  * By using, modifying, or distributing this software, you agree to the terms and conditions of the AGPL.
  * A copy of the license can be found in the LICENSE file.
- *
- * When useful we use the hungarian notation for variable names.
- * It helps quickly know if a variable is an array, a number, curve point,
- * hex string, or something else.
- * Here are the prefixes:
- *
- * - a for arrays
- * - b for booleans
- * - h for hexadecimal strings
- * - s for other strings
- * - n for BigInt
- * - p for curve points
- * - z for zero-knowledge proofs
- * - e for ElGamal ciphertexts
  */
 
 import { createApp } from "vue";
