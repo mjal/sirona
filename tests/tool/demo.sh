bb869e48acf37027304f58b65a621a36c53f7585
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-08-28 19:16:56 +0200

    Generate credentials in demo.sh

--- tests/tool/demo.sh
+++ tests/tool/demo.sh
@@ -23,6 +23,20 @@ DIR=tests/tool/data/$UUID
 mkdir -p $DIR
 cd $DIR
 
+# Generate credentials
+cat > voters.txt <<EOF
+voter1@example.com,voter1,1000000000
+voter2@example.com,voter2,2000000000
+voter3@example.com,voter3,3000000000
+voter4@example.com,voter4,4000000000
+voter5@example.com,voter5,90000000000
+EOF
+sirona setup generate-credentials --uuid $UUID --file voters.txt | tee generate-credentials.out
+mv *.pubcreds public_creds.json
+mv *.privcreds private_creds.json
+paste <(jq --raw-output 'keys_unsorted[]' < private_creds.json) <(jq --raw-output '.[]' < private_creds.json) > private_creds.txt
+
+# Generate trustee keys
 echo -n "" > public_keys.json
 
 sirona setup generate-trustee-key
