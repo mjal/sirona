54c077f7da0fbebc3d025f79256012f818f1f8e9
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-12-03 09:44:39 +0100

    Export Trustee and Election

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as Election from "./dist/Election.js";
+import * as Trustee from "./dist/Trustee.js";
+import sjcl from "sjcl";
+
+export { Trustee, Election };
+
+export default {
+  Trustee,
+  Election,
+  sjcl
+};
