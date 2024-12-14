fd305be47fcbf581910eac28e288b5481a65fb60
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-12-14 14:17:29 +0100

    DecryptionProof.generate

--- src/proofs/DecryptionProof.ts
+++ src/proofs/DecryptionProof.ts
@@ -2,6 +2,7 @@ import { Hdecrypt } from "./../math";
 import * as Proof from "./../Proof";
 import * as ElGamal from "./../ElGamal";
 import * as Point from "./../Point";
+import * as Zq from "../Zq";
 
 export function verify(
   S: string,
@@ -13,4 +14,19 @@ export function verify(
   const A = Point.commit(Point.g, y, proof);
   const B = Point.commit(e.alpha, factor, proof);
   return Hdecrypt(S, A, B) === proof.challenge;
+}
+
+export function generate(
+  S: string,
+  e: ElGamal.t,
+  x: bigint
+) {
+  const w = Zq.rand();
+  const A = Point.g.multiply(w);
+  const B = e.alpha.multiply(w);
+
+  const challenge = Hdecrypt(S, A, B);
+  const response = Zq.mod(w - x * challenge);
+
+  return { challenge, response };
 }
