9902c292dad04f6a01d5cceac86183ef97597af5
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-12-18 17:54:41 +0100

    Fix export format + export sjcl

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import * as Ballot from "./dist/Ballot.js";
 import * as Credential from "./dist/Credential.js";
 import sjcl from "sjcl";
 
-// TODO: Only keep the export format used by scrutin
-export { Trustee, Election, Ballot, Credential };
-export default {
+export {
   Trustee,
   Election,
   Ballot,
