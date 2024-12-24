f50f86c9a67ca0fb0bb44ed19a56dc114e69003e
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-12-24 14:24:39 +0100

    Export more modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import * as Trustee from "./dist/Trustee.js";
 import * as Election from "./dist/Election.js";
 import * as Ballot from "./dist/Ballot.js";
 import * as Credential from "./dist/Credential.js";
+import * as EncryptedTally from "./dist/EncryptedTally.js";
+import * as Setup from "./dist/Setup.js";
 import sjcl from "sjcl";
 
 export {
@@ -9,5 +11,7 @@ export {
   Election,
   Ballot,
   Credential,
+  EncryptedTally,
+  Setup,
   sjcl
 };
