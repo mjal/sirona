d2d88d2aad6546ecb67f9bcc44133aede86b18d8
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-12-27 19:56:27 +0100

    update test-lib

--- test-lib.ts
+++ test-lib.ts
@@ -6,7 +6,7 @@ import * as Credential from "./src/Credential";
 import * as EncryptedTally from "./src/EncryptedTally";
 import * as PartialDecryption from "./src/PartialDecryption";
 import * as Setup from "./src/Setup";
-import sjcl from "sjcl";
+import * as Result from "./src/Result";
 
 let title = "My title"
 let description = "My description"
@@ -29,10 +29,13 @@ let priv1 = Credential.generatePriv()
 let cred1 = Credential.derive(election.uuid, priv1)
 let priv2 = Credential.generatePriv()
 let cred2 = Credential.derive(election.uuid, priv2)
+let priv3 = Credential.generatePriv()
+let cred3 = Credential.derive(election.uuid, priv3)
 
 let credentials = [
   cred1.hPublicCredential,
-  cred2.hPublicCredential
+  cred2.hPublicCredential,
+  cred3.hPublicCredential
 ]
 
 let setup : Setup.t = {
@@ -43,8 +46,9 @@ let setup : Setup.t = {
 console.log(setup)
 
 let ballots : Ballot.t[] = []
-ballots.push(Ballot.generate(setup, priv1, [[0,1,0]]))
-ballots.push(Ballot.generate(setup, priv2, [[0,1,0]]))
+ballots.push(Ballot.generate(setup, priv1, [[0,0,1]]))
+ballots.push(Ballot.generate(setup, priv2, [[0,0,1]]))
+ballots.push(Ballot.generate(setup, priv3, [[1,0,0]]))
 console.log(ballots)
 
 console.log(ballots[0].answers[0].choices)
@@ -54,3 +58,9 @@ console.log(et)
 
 let pd = PartialDecryption.generate(setup, et, 1, x)
 console.log(pd)
+console.log(pd.payload.decryption_factors)
+console.log(pd.payload.decryption_proofs)
+
+let res = Result.generate(setup, et, [pd],  [])
+console.log(res)
+// {result: [[0,2,0]]}, 
